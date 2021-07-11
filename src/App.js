1463c527fd1b4fcb1c27e0f08f317ec6d708ac21
Shubham <1928196@kiit.ac.in>
2021-07-11 15:32:31 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => (
+  <>
+    <h1>Welcome to React Parcel Micro App!</h1>
+    <p>Hard to get more minimal.</p>
+    <small>Hi, I'm going to make a tic tac toe app very soon</small>
+  </>
+);
